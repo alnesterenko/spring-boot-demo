c648a419d82459f286ff8c64c84c112d1edec591
alnesterenko <alzpupkin@gmail.com>
2025-07-24 10:06:37 +0300

    10.3.3. Управление исключениями на уровне конечной точки. Вариант 2 (с использованием совета с перехватчиком исключений @ExceptionHandler). С моей небольшой доработкой.

---
+++ src/main/java/com/example/demo/rest/manageexception2/exceptions/NotEnoughMoneyException.java
@@ -0,0 +1,4 @@
+package com.example.demo.rest.manageexception2.exceptions;
+
+public class NotEnoughMoneyException extends RuntimeException {
+}
---
+++ src/main/java/com/example/demo/rest/manageexception2/model/ErrorDetails.java
@@ -0,0 +1,14 @@
+package com.example.demo.rest.manageexception2.model;
+
+public class ErrorDetails {
+
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
